0bb04f15486fbceeb91da0ca30721641baa0728d
AshwiniSG2404 <ashwinisg044@gmail.com>
2025-03-24 14:14:09 +0530

    Added new testcase for deleting product

---
+++ Ninza_CRMAutomationFramework/src/test/java/createProduct/DeleteProduct.java
@@ -0,0 +1,12 @@
+package createProduct;
+
+import org.testng.annotations.Test;
+
+public class DeleteProduct {
+
+	@Test
+	public void deleteTest()
+	{
+		System.out.println("Product deleted successfully");
+	}
+}
