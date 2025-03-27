f16aaab722aeb3073aa9a01d2d52f119c09c6a77
AshwiniSG2404 <ashwinisg044@gmail.com>
2025-03-27 12:03:44 +0530

    My first Commit

---
+++ DummyProject/src/test/java/Package1/TestCase1.java
@@ -0,0 +1,12 @@
+package Package1;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+
+	@Test
+	public void ReadTest()
+	{
+		System.out.println("Read test executed Successfully");
+	}
+}
---
+++ DummyProject/src/test/java/Package1/Testcase2.java
@@ -0,0 +1,12 @@
+package Package1;
+
+import org.testng.annotations.Test;
+
+public class Testcase2 {
+
+	@Test
+	public void WriteTest()
+	{
+		System.out.println("Write test executed Successfully");
+	}
+}
